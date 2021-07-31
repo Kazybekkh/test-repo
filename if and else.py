387ab1c8a1fff2d46937abe81d7ec61116541741
Kazybek Khairulla <76276851+Kazybekkh@users.noreply.github.com>
2021-07-31 23:57:53 +0800

    Add files via upload

---
+++ if and else.py
@@ -0,0 +1,8 @@
+n=int(input('type a number for n '))
+a=int(input('type a number fot a '))
+if a<n:
+    print ('Black in Black')
+elif a>n:
+    print ('Shoot to Thrill')
+else:
+    print ('Highway to HEll')
