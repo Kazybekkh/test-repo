1163949e57bd080464fe9cf8c28deef867bd4d1b
Kazybekkh <kazybekkhairulla@gmail.com>
2021-08-06 16:15:02 +0800

    Another new commit has been made

--- if and else.py
+++ if and else.py
@@ -1,8 +1,8 @@
 n=int(input('type a number for n '))
 a=int(input('type a number fot a '))
 if a<n:
-    print ('Black in Black')
+    print ('Black in Black, updated')
 elif a>n:
-    print ('Shoot to Thrill')
+    print ('Shoot to Thrill, updated')
 else:
-    print ('Highway to HEll')
+    print ('Highway to HEll, updated')
